5d072d25d8dc24681579fd7f23d7d6d93ad05b33
Corn070201 <corn070201@gmail.com>
2021-12-29 23:25:55 +0700

    Init source webAPI

---
+++ MyWebApiApp/MyWebApiApp/Controllers/HangHoaController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hanghoas = new List<HangHoa>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hanghoas);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                //Linq[object] Query
+                var hanghoa = hanghoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hanghoa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(hanghoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+     
+        }
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVM)
+        {
+            var hanghoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hangHoaVM.TenHangHoa,
+                DonGia = hangHoaVM.DonGia
+            };
+            hanghoas.Add(hanghoa);
+            return Ok(new
+            {
+                Success = true, Data = hanghoa
+            });
+        }
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, HangHoa hanghoaEdit)
+        {
+            try
+            {
+                //Linq[object] Query
+                var hanghoa = hanghoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hanghoa == null)
+                {
+                    return NotFound();
+                }
+                if(id != hanghoa.MaHangHoa.ToString())
+                {
+                    return BadRequest();
+                }
+                //update
+                hanghoa.TenHangHoa = hanghoaEdit.TenHangHoa;
+                hanghoa.DonGia = hanghoaEdit.DonGia;
+                return Ok();
+                //return Ok(hanghoa);
+            }
+
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Remove(string id)
+        {
+            try
+            {
+                //Linq[object] Query
+                var hanghoa = hanghoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hanghoa == null)
+                {
+                    return NotFound();
+                }
+
+                //update
+                hanghoas.Remove(hanghoa);
+                return Ok();
+                //return Ok(hanghoa);
+            }
+
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ MyWebApiApp/MyWebApiApp/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApiApp.Servises;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IHangHoaResposity _hangHoaResposity;
+
+        public ProductsController(IHangHoaResposity hangHoaResposity)
+        {
+            _hangHoaResposity = hangHoaResposity;
+        }
+        [HttpGet]
+        public IActionResult GettAllProducts(string search, double? from, double? to, string sortBy, int page = 1)
+        {
+            try
+            {
+                List<Models.HangHoaModel> result = _hangHoaResposity.GetAll(search, from, to, sortBy, page);
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest("We can not get the product");
+            }
+        }
+    }
+}
